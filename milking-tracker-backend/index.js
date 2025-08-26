8af2c5d85f2237809e0dd662c6f286e0f10719dc
Mananjot17 <mananjotsingh17@gmail.com>
2025-08-26 23:41:40 +0530

    cors

--- milking-tracker-backend/index.js
+++ milking-tracker-backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors({ origin: process.env.CORS_ORIGIN }));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/milking-sessions", milkingSessionRoutes);
